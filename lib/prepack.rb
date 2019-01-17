217f4933044ee78906781a0ee4279b97c1d13c75
Kevin Deisz <kevin.deisz@gmail.com>
2019-01-17 10:55:44 -0500

    Set up passing infra

--- lib/prepack.rb
+++ lib/prepack.rb
@@ -1,6 +1,16 @@
-require "prepack/version"
+require 'prepack/version'
 
 module Prepack
-  class Error < StandardError; end
-  # Your code goes here...
+  class << self
+    attr_accessor :passes
+
+    def process(source)
+      passes.each do |pass|
+        source = pass.process(source)
+      end
+      source
+    end
+  end
+
+  self.passes = []
 end
