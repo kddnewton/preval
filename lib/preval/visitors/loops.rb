b41bcc2d08dcf6d1a13d5850a0aeba7fa39f2a21
Kevin Deisz <kevin.deisz@gmail.com>
2019-04-19 04:00:02 -0400

    Better documentation for loops

--- lib/preval/visitors/loops.rb
+++ lib/preval/visitors/loops.rb
@@ -3,36 +3,33 @@
 module Preval
   class Visitors
     class Loops < Visitor
-      module TrueNode
-        refine Node do
-          def true?
-            is?(:var_ref) && starts_with?(:@kw) && body[0].body == 'true'
-          end
-        end
-      end
-
-      using TrueNode
-
       def on_for(node)
-        sexp = Parser.parse(<<~CODE)
+        ast = Parser.parse(<<~CODE)
           #{node.source(1)}.each do |#{node.source(0)}|
             #{node.source(2)}
           end
         CODE
 
-        node.update(:stmts_add, sexp.body[0].body)
+        node.update(:stmts_add, ast[0].body)
       end
 
       def on_while(node)
-        return unless node.body[0].true?
+        # auto replace while true with loop do
+        if node[0].is?(:var_ref) &&
+             # the predicate to the while is a variable reference
+           node[0, 0].is?(:@kw) &&
+             # the variable reference is a keyword
+           node[0, 0].body == 'true'
+             # the keyword is "true"
 
-        sexp = Parser.parse(<<~CODE)
-          loop do
-            #{node.source(1)}
-          end
-        CODE
+          ast = Parser.parse(<<~CODE)
+            loop do
+              #{node.source(1)}
+            end
+          CODE
 
-        node.update(:stmts_add, sexp.body[0].body)
+          node.update(:stmts_add, ast[0].body)
+        end
       end
     end
   end
